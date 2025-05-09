097b7cdf892ed330d31bcddb045c76698822b3e2
Andres Camilo Solano Pantoja <andresolano.12.651@gmail.com>
2025-05-09 17:07:00 -0500

    feat(domain): create base entities with auditing and category support

---
+++ skillJas.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace SkillJas.Domain.Common;
+
+public abstract class AuditableEntity
+{
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedDate { get; set; }
+}
